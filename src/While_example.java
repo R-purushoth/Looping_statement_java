4212564a5f4d0a2546018b6979ef3ffc215cbe9d
shothp345@gmail.com <shothp345@gmail.com>
2025-07-13 10:10:30 +0530

    git commit -m "first commit"

--- src/While_example.java
+++ src/While_example.java
@@ -2,6 +2,7 @@ public class While_example {
     public static void main(String[] args) {
         // While loop example
         // While is an entry level loop that means first it check the condition and then perform the operation
+        // this code to print first 10 numbers
         int a=1;
         while(a<=10){
             System.out.println(a);
