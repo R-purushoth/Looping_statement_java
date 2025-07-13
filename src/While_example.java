2d5f7d94bec6cc2fe9200fcb2c010ed997b1cdd2
shothp345@gmail.com <shothp345@gmail.com>
2025-07-13 10:04:50 +0530

    first commit

---
+++ src/While_example.java
@@ -0,0 +1,11 @@
+public class While_example {
+    public static void main(String[] args) {
+        // While loop example
+        // While is an entry level loop that means first it check the condition and then perform the operation
+        int a=1;
+        while(a<=10){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
