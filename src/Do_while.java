7f313fae191afc514476acc543f72b4c50be1343
shothp345@gmail.com <shothp345@gmail.com>
2025-07-28 09:21:02 +0530

    first commit

---
+++ src/Do_while.java
@@ -0,0 +1,10 @@
+public class Do_while {
+    public static void main(String[] args) {
+        int a=5;
+        do{
+            System.out.println(a); // it's print the statement first and check the condition at end.
+            // that my do-while called exit level loop.
+            a++;
+        }while(a < 5);
+    }
+}
